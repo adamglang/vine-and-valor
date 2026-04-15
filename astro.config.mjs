c79f42215ee0482658fa2b3e2fe202824dd22a51
adamglang <adam.lang.sde@gmail.com>
2026-04-15 02:32:34 -0700

    stand up admin page and github login

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,7 +60,6 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  redirects: { "/admin": "/admin/index.html" },
 
   image: { service: sharpImageService() },
   vite: { plugins: [tailwindcss()] },
